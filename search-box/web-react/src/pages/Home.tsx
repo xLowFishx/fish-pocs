949455b6613daa51f9cf199b8ab5ea9831673521
Lowfish air <11095094+xLowFishx@users.noreply.github.com>
2026-06-25 15:15:48 -0600

    FP-003 [Encapsulating styles]

--- search-box/web-react/src/pages/Home.tsx
+++ search-box/web-react/src/pages/Home.tsx
@@ -2,9 +2,30 @@ import Spacer from '@app/components/Spacer'
 import MainLogo from '@app/components/MainLogo'
 import SearchBox from '@app/features/SearchBox'
 
+function HomeStyles() {
+  return <style href="center" precedence='component'>
+    {`
+      #center {
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+        place-content: center;
+        place-items: center;
+        flex-grow: 1;
+
+        @media (max-width: 1024px) {
+          padding: 32px 20px 24px;
+          gap: 18px;
+        }
+      }
+    `}
+  </style>
+}
+
 export default function Home() {
   return (
     <>
+      <HomeStyles />
       <Spacer />
       <section id="center">
         <MainLogo />
@@ -12,24 +33,6 @@ export default function Home() {
         <SearchBox placeholderText="Type a star wars character" />
       </section>
       <Spacer />
-
-      <style href="center" precedence='component'>
-        {`
-          #center {
-            display: flex;
-            flex-direction: column;
-            gap: 25px;
-            place-content: center;
-            place-items: center;
-            flex-grow: 1;
-
-            @media (max-width: 1024px) {
-              padding: 32px 20px 24px;
-              gap: 18px;
-            }
-          }
-        `}
-      </style>
     </>
   )
 }
